c8d861d809767e0afdf6763506446901b6a69ba8
68Kamil68 <tret1lus@gmail.com>
2019-03-12 14:08:59 +0100

    poprawione

--- lab1/dlawszystkich1.sh
+++ lab1/dlawszystkich1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-url=$(awk -F = '{print $2}' lista.txt)
-for i in $(cat lista.txt);
+
+for i in `cat lista.txt`;
 do
-    echo $url${i}
-    wget -q "${url}${i}" 
-done    
+    echo $i
+    wget -q $i 
+done   
--- lab1/zad1.sh
+++ lab1/zad1.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-for i in `ls -l $1| awk '{ if ( $1~/x/){print $NF}}'` # | wysyla ls -l do awk $1 - pozwolenia jesli x wyswietla ostatnia pozycje w tabeli - nazwe pliku
+for i in `ls -l $1`
 do
+    if [ -x $i ]
+    then
     echo $i
+    fi
 done    
